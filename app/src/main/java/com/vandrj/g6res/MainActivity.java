ce832092e47622a3733117f81121e7a3ade691e7
VAndrJ <vandrjios@gmail.com>
2020-02-08 01:42:09 +0200

    Remove unused

--- app/src/main/java/com/vandrj/g6res/MainActivity.java
+++ app/src/main/java/com/vandrj/g6res/MainActivity.java
@@ -7,8 +7,6 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -69,28 +67,6 @@ public class MainActivity extends AppCompatActivity {
                 runShellCommand("wm size 720x1440\nwm density 320\nwm size 225x450\nwm density 100", v);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void runShellCommand(String command, View v) {
